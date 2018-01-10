4539fafbcd5fe1d0c3ad4d660d5ad50ffb82e6ca
Mathcat <mathnet@qq.com>
2018-01-10 10:56:58 +0800

    FtpOperator改为FtpConnetion。完善一些操作 增加FtpConnectionPool，尚未实现

--- spin/src/main/java/org/spin/core/util/ftp/FtpConnection.java
+++ spin/src/main/java/org/spin/core/util/ftp/FtpConnection.java
@@ -2,7 +2,7 @@ package org.spin.core.util.ftp;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPListParseEngine;
+import org.apache.commons.net.ftp.FTPFileFilter;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.slf4j.Logger;
@@ -18,19 +18,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
- * FTP工具类
- * <p>非线程安全</p>
+ * FTP客户端连接
+ * <p>
+ * 非线程安全，避免多线程对同一连接同时进行操作<br/>
+ * 一个连接只能<strong>顺序地依次执行</strong>一组FTP动作。
+ * 同时使用同一连接进行FTP操作，或者在上一操作尚未完成的情况下进行下一操作，可能导致无法预计的问题
+ * </p>
  * <p>Created by xuweinan on 2016/8/22.</p>
  *
  * @author xuweinan
  * @version V1.1
  */
-public class FtpOperator implements AutoCloseable {
-    private static final Logger logger = LoggerFactory.getLogger(FtpOperator.class);
+public class FtpConnection implements AutoCloseable {
+    private static final Logger logger = LoggerFactory.getLogger(FtpConnection.class);
     private static final Pattern protocalPattern = Pattern.compile("^(ftp[s]?)://(.+:.+@)?([^:]+)(:\\d{2,5})?$", Pattern.CASE_INSENSITIVE);
 
     private String name;
@@ -40,8 +50,9 @@ public class FtpOperator implements AutoCloseable {
     private String host;
     private int port = 21;
 
-    private String localCharset = System.getProperty("sun.jnu.encoding");
-    private String serverCharset = "ISO-8859-1";
+    //    private String serverCharset = System.getProperty("sun.jnu.encoding");
+    private Charset serverCharset = Charset.forName("GBK");
+    private Charset ftpCharset = Charset.forName("ISO-8859-1");
 
     private FTPClient client;
 
@@ -50,7 +61,7 @@ public class FtpOperator implements AutoCloseable {
      *
      * @param url 标准FTP连接地址，形如：ftp://用户名:密码@服务器ip地址
      */
-    public FtpOperator(String url) {
+    public FtpConnection(String url) {
         Matcher matcher = protocalPattern.matcher(url);
         if (!matcher.matches())
             throw new SimplifiedException("FTP连接URL格式错误");
@@ -91,7 +102,7 @@ public class FtpOperator implements AutoCloseable {
      * @param password 密码
      * @param host     主机
      */
-    public FtpOperator(Protocal protocal, String userName, String password, String host) {
+    public FtpConnection(Protocal protocal, String userName, String password, String host) {
         this(protocal, userName, password, host, 21);
     }
 
@@ -102,7 +113,7 @@ public class FtpOperator implements AutoCloseable {
      * @param password 密码
      * @param host     主机
      */
-    public FtpOperator(String userName, String password, String host) {
+    public FtpConnection(String userName, String password, String host) {
         this(Protocal.FTP, userName, password, host, 21);
     }
 
@@ -115,7 +126,7 @@ public class FtpOperator implements AutoCloseable {
      * @param host     主机
      * @param port     端口
      */
-    public FtpOperator(Protocal protocal, String userName, String password, String host, int port) {
+    public FtpConnection(Protocal protocal, String userName, String password, String host, int port) {
         Protocal p = null == protocal ? Protocal.FTP : protocal;
         this.protocal = p;
         this.host = host;
@@ -136,9 +147,10 @@ public class FtpOperator implements AutoCloseable {
      * 执行FTP指令
      *
      * @param command 指令文本
+     * @param args    参数
      * @return 状态码
      */
-    public int sendCommands(String command, String args) {
+    public int sendCommand(String command, String args) {
         this.connect();
         try {
             return client.sendCommand(command, args);
@@ -149,39 +161,68 @@ public class FtpOperator implements AutoCloseable {
     }
 
     /**
-     * 列出指定路径下所有远程文件
+     * 执行FTP指令
      *
-     * @param path 远程路径
+     * @param command 指令文本
+     * @return 状态码
      */
-    public void listRemoteAllFiles(String path) {
+    public int sendCommand(String command) {
+        return sendCommand(command, null);
+    }
+
+    /**
+     * 列出指定远程目录下所有文件(及目录)名称，目录名称以"/"结尾
+     *
+     * @param remotePath 远程目录
+     * @param filter     过滤器
+     * @return 文件名列表
+     */
+    public List<String> listFileNames(String remotePath, FTPFileFilter filter) {
         this.connect();
         try {
-            FTPListParseEngine f = client.initiateListParsing(path);
-
-            while (f.hasNext()) {
-                FTPFile[] files = f.getNext(5);
-                for (FTPFile file : files) {
-                    if (file.isDirectory() && !file.getName().equals(".") && !file.getName().equals("..")) {
-                        logger.info(File.separator + file.getName());
-                        listRemoteAllFiles(path + File.separator + file.getName());
-                    } else if (!file.getName().equals(".") && !file.getName().equals("..")) {
-                        logger.info(file.getName());
-                    }
-                }
-            }
+            return Arrays.stream(client.listFiles(remotePath, filter)).map(f -> f.isDirectory() ? f.getName() + "/" : f.getName())
+                .filter(n -> !"./".equals(n) && !"../".equals(n)).collect(Collectors.toList());
         } catch (IOException e) {
-            logger.error("列出文件失败: ", e);
+            throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "无法列出远程目录下的文件", e);
         }
     }
 
-    public void disFile(FTPFile file, String path) {
+    /**
+     * 列出指定远程目录及子目录下所有文件(及目录)名称，目录名称以"/"结尾
+     *
+     * @param remotePath 远程目录
+     * @param filter     过滤器
+     * @return 文件名列表
+     */
+    public List<String> listAllFileNames(String remotePath, FTPFileFilter filter) {
         this.connect();
-        if (file.isDirectory() && !file.getName().equals(".") && !file.getName().equals("..")) {
-            logger.info(File.separator + file.getName());
-            listRemoteAllFiles(path + File.separator + file.getName());
-        } else if (!file.getName().equals(".") && !file.getName().equals("..")) {
-            logger.info(file.getName());
+        Queue<String> dirs = new LinkedList<>();
+        List<String> fileNames = new LinkedList<>();
+        dirs.add(remotePath);
+        String path;
+        FTPFile[] ftpFiles;
+        FTPFile f;
+        while (!dirs.isEmpty()) {
+            path = dirs.poll();
+            try {
+                ftpFiles = client.listFiles(path, filter);
+                for (int i = 0; i < ftpFiles.length; i++) {
+                    f = ftpFiles[i];
+                    if (!".".equals(f.getName()) && "..".equals(f.getName())) {
+                        if (f.isDirectory()) {
+                            dirs.add(path + "/" + f.getName());
+                            fileNames.add(f.getName() + "/");
+                        } else {
+                            fileNames.add(f.getName());
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "无法列出远程目录下的文件", e);
+            }
         }
+        return fileNames;
     }
 
     /**
@@ -355,67 +396,119 @@ public class FtpOperator implements AutoCloseable {
     @Override
     public void close() {
         try {
-            if (client != null && client.isConnected()) {
+            if (isAvaliable()) {
                 client.logout();
             }
         } catch (Exception e) {
-            logger.error("登出ftp服务器失败", e);
+            logger.error("登出FTP服务器失败", e);
         } finally {
-            logger.info("关闭ftp服务器连接");
+            logger.info("关闭FTP服务器连接");
             try {
-                if (client != null) {
+                if (client.isConnected()) {
                     client.disconnect();
                 }
             } catch (Exception e) {
-                logger.info("关闭ftp服务器连接异常", e);
+                logger.error("关闭FTP服务器连接异常", e);
             }
         }
     }
 
-    private void connect() {
-        if (null == client) {
-            throw new SimplifiedException("FTP客户端未初始化");
-        }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Protocal getProtocal() {
+        return protocal;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public Charset getServerCharset() {
+        return serverCharset;
+    }
+
+    public void setServerCharset(Charset serverCharset) {
+        this.serverCharset = serverCharset;
+        close();
+        connect();
+    }
+
+    public Charset getFtpCharset() {
+        return ftpCharset;
+    }
+
+    private String encodeStr(String value) {
+        return new String(value.getBytes(serverCharset), ftpCharset);
+    }
+
+    public boolean isAvaliable() {
         if (client.isConnected()) {
+            try {
+                return FTPReply.isPositiveCompletion(client.sendCommand("NOOP"));
+            } catch (IOException e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void connect() {
+        if (isAvaliable()) {
             return;
         }
-        client.setDefaultPort(port);
+        if (!client.isConnected()) {
+            client.setDefaultPort(port);
 
-        try {
-            client.connect(host);
-        } catch (IOException e) {
-            throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP连接失败");
+            try {
+                client.connect(host);
+            } catch (IOException e) {
+                throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP连接失败: " + host + ":" + port);
+            }
         }
         if (StringUtils.isNotEmpty(userName)) {
             try {
                 if (!client.login(userName, password)) {
-                    throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP登录失败");
+                    String replyString = client.getReplyString();
+                    try {
+                        client.disconnect();
+                    } catch (IOException e) {
+                        throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP访问被拒绝，断开连接失败");
+                    }
+                    throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP登录失败: " + host + ":" + port + " [" + replyString + "]");
                 }
             } catch (IOException e) {
-                throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP登录失败");
-            }
-        }
+                throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP登录失败: " + host + ":" + port, e);
 
-        int reply = client.getReplyCode();
-
-        if (!FTPReply.isPositiveCompletion(reply)) {
-            try {
-                client.disconnect();
-            } catch (IOException e) {
-                throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP访问被拒绝，断开连接失败");
             }
-            throw new SimplifiedException(ErrorCode.NETWORK_EXCEPTION, "FTP访问被拒绝: " + host + ":" + port);
         }
 
         client.setDataTimeout(30000);
-        // 开启服务器对UTF-8的支持，如果服务器支持就用UTF-8编码，否则就使用本地编码.
+        // 开启服务器对UTF-8的支持，如果服务器支持就用UTF-8编码，否则就使用默认编码.
         try {
             if (FTPReply.isPositiveCompletion(client.sendCommand("OPTS UTF8", "ON"))) {
-                localCharset = "UTF-8";
+                serverCharset = Charset.forName("UTF-8");
             }
         } catch (IOException e) {
         }
-        client.setControlEncoding(localCharset);
+        client.setControlEncoding(serverCharset.name());
         client.enterLocalPassiveMode();
         try {
             if (!client.setFileType(FTPClient.BINARY_FILE_TYPE))
---
+++ spin/src/main/java/org/spin/core/util/ftp/FtpConnectionPool.java
@@ -0,0 +1,9 @@
+package org.spin.core.util.ftp;
+
+/**
+ * <p>Created by xuweinan on 2018/1/10.</p>
+ *
+ * @author xuweinan
+ */
+public class FtpConnectionPool {
+}
