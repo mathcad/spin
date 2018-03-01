c831d46371cefee3decaad62759485520f980f71
Mathcat <mathnet@qq.com>
2018-03-01 11:54:59 +0800

    JSON适配器工厂调整，避免多线程问题

--- spin/src/main/java/org/spin/enhance/gson/SpinTypeAdapterFactory.java
+++ spin/src/main/java/org/spin/enhance/gson/SpinTypeAdapterFactory.java
@@ -6,7 +6,6 @@ import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
 import org.spin.core.util.ClassUtils;
 import org.spin.core.util.ConstructorUtils;
-import org.spin.core.util.StringUtils;
 import org.spin.enhance.gson.adapter.LocalDateTimeTypeAdapter;
 import org.spin.enhance.gson.adapter.LocalDateTypeAdapter;
 import org.spin.enhance.gson.adapter.LocalTimeTypeAdapter;
@@ -15,8 +14,6 @@ import org.spin.enhance.gson.adapter.UserEnumTypeAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Gson TypeAdapterFactory
@@ -29,6 +26,8 @@ public class SpinTypeAdapterFactory implements TypeAdapterFactory {
     private String localDatePatern;
     private String localTimePatern;
     private static List<MatchableTypeAdapter<?>> typeAdapters = new ArrayList<>();
+    private boolean inited;
+    private final Object lock = new Object();
 
     public SpinTypeAdapterFactory(String datePattern, String localDatePatern, String localTimePatern) {
         this.datePattern = datePattern;
@@ -37,27 +36,34 @@ public class SpinTypeAdapterFactory implements TypeAdapterFactory {
     }
 
     @SuppressWarnings("unchecked")
-    private <T> void init(Gson gson, TypeToken<T> type) {
-        try {
-            Class<MatchableTypeAdapter> cls = (Class<MatchableTypeAdapter>) ClassUtils.getClass("org.spin.enhance.gson.adapter.HibernateProxyTypeAdapter");
-            typeAdapters.add(ConstructorUtils.invokeConstructor(cls, gson));
-            cls = (Class<MatchableTypeAdapter>) ClassUtils.getClass("org.spin.enhance.gson.adapter.HibernatePersistentBagTypeAdapter");
-            typeAdapters.add(cls.getConstructor().newInstance());
-        } catch (Exception ignore) {
-            // ignore
+    private void init(Gson gson) {
+        if (!inited) {
+            synchronized (lock) {
+                if (!inited) {
+                    inited = true;
+                    try {
+                        Class<MatchableTypeAdapter> cls = (Class<MatchableTypeAdapter>) ClassUtils.getClass("org.spin.enhance.gson.adapter.HibernateProxyTypeAdapter");
+                        typeAdapters.add(ConstructorUtils.invokeConstructor(cls, gson));
+                        cls = (Class<MatchableTypeAdapter>) ClassUtils.getClass("org.spin.enhance.gson.adapter.HibernatePersistentBagTypeAdapter");
+                        typeAdapters.add(cls.getConstructor().newInstance());
+                    } catch (Exception ignore) {
+                        // ignore
+                    }
+                    typeAdapters.add(new LocalDateTimeTypeAdapter(datePattern));
+                    typeAdapters.add(new LocalDateTypeAdapter(localDatePatern));
+                    typeAdapters.add(new LocalTimeTypeAdapter(localTimePatern));
+                    typeAdapters.add(new LongTypeAdapter());
+                    typeAdapters.add(new UserEnumTypeAdapter());
+                }
+            }
         }
-        typeAdapters.add(new LocalDateTimeTypeAdapter(datePattern));
-        typeAdapters.add(new LocalDateTypeAdapter(localDatePatern));
-        typeAdapters.add(new LocalTimeTypeAdapter(localTimePatern));
-        typeAdapters.add(new LongTypeAdapter());
-        typeAdapters.add(new UserEnumTypeAdapter());
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public final <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
         if (typeAdapters.isEmpty()) {
-            init(gson, type);
+            init(gson);
         }
         return (TypeAdapter<T>) typeAdapters.stream().filter(t -> t.isMatch(type)).findFirst().orElse(null);
     }
