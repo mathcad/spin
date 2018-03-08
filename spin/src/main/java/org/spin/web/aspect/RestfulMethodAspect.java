ddf069b1956c3774a45940717288c61f46a24395
Mathcat <mathnet@qq.com>
2018-03-08 15:25:56 +0800

    解决RestfulMethod对懒加载对象自动处理的问题

--- spin/src/main/java/org/spin/web/aspect/RestfulMethodAspect.java
+++ spin/src/main/java/org/spin/web/aspect/RestfulMethodAspect.java
@@ -4,12 +4,14 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.spin.data.core.IEntity;
 import org.spin.data.util.EntityUtils;
+import org.spin.web.annotation.RestfulMethod;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 /**
  * Restful方法切面
@@ -20,19 +22,21 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class RestfulMethodAspect implements Ordered {
-    private static final Logger logger = LoggerFactory.getLogger(RestfulMethodAspect.class);
 
     @Pointcut("execution(public * *.*(..)) && @annotation(org.spin.web.annotation.RestfulMethod)")
     private void restfulMethod() {
+        // no content
     }
 
     @Around("restfulMethod()")
     public Object restfulAround(ProceedingJoinPoint joinPoint) throws Throwable {
         Object r = joinPoint.proceed();
+        Method rMethod = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        RestfulMethod anno = rMethod.getAnnotation(RestfulMethod.class);
         if (null == r) {
             return null;
         } else if (r instanceof IEntity<?>) {
-            return EntityUtils.getDTO(r, 1);
+            return EntityUtils.getDTO(r, anno.fetchDepth());
         }
         return r;
     }
