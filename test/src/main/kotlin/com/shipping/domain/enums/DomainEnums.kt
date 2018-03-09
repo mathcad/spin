8780e5352f472baba3dfecfd3ae3ba84bd27ea27
Mathcat <mathnet@qq.com>
2018-03-09 16:32:10 +0800

    no message

---
+++ test/src/main/kotlin/com/shipping/domain/enums/DomainEnums.kt
@@ -0,0 +1,61 @@
+package com.shipping.domain.enums
+
+import org.spin.core.annotation.UserEnum
+import org.spin.data.core.UserEnumColumn
+
+/**
+ * 系统功能类型
+ *
+ * @author xuweinan
+ */
+@UserEnum("系统功能类型")
+enum class FunctionTypeE(private val value: Int) : UserEnumColumn {
+    MEMU(1), API(2);
+
+    override fun getValue(): Int {
+        return value
+    }
+}
+
+/**
+ * 组织机构类型
+ *
+ * @author xuweinan
+ */
+@UserEnum("系统功能类型")
+enum class OrganizationTypeE(private val value: Int, val desc: String) : UserEnumColumn {
+    GROUP(1, "集团"), CONPANY(2, "公司"), SUBSIDIARY(3, "子公司"), BRANCH(4, "分公司"), DIVISION(5, "事业部"), REGION(6, "大区"), DEPARTMENT(7, "部门"), TEAM(8, "团队");
+
+    override fun getValue(): Int {
+        return this.value
+    }
+}
+
+/**
+ * 区域类型
+ *
+ * @author xuweinan
+ */
+@UserEnum("区域类型")
+enum class RegionTypeE(private val value: Int) : UserEnumColumn {
+    PROVINCE(1), CITY(2), DISTRICT(3);
+
+    override fun getValue(): Int {
+        return value
+    }
+}
+
+/**
+ * 用户类型
+ * Created by xuweinan on 2016/12/4.
+ *
+ * @author xuweinan
+ */
+@UserEnum("用户类型")
+enum class UserTypeE(private val value: Int) : UserEnumColumn {
+    普通用户(2), 微信用户(3);
+
+    override fun getValue(): Int {
+        return value
+    }
+}
