d5f1eed9333143053029653f38667091d30c101f
Mathcat <mathnet@qq.com>
2018-09-28 21:04:40 +0800

    实现限流功能

--- spin-core/src/test/java/org/spin/core/concurrent/RateLimiterTest.java
+++ spin-core/src/test/java/org/spin/core/concurrent/RateLimiterTest.java
@@ -1,5 +1,9 @@
 package org.spin.core.concurrent;
 
+import org.spin.core.util.DateUtils;
+
+import java.util.Date;
+
 /**
  * TITLE
  * <p>DESCRIPTION</p>
@@ -9,18 +13,23 @@ package org.spin.core.concurrent;
  */
 public class RateLimiterTest {
 
-    static RateLimiter limiter = RateLimiter.create(3);
+    static RateLimiter limiter = RateLimiter.create(10);
 
     public static void main(String[] args) throws InterruptedException {
         int i = 0;
+        limiter.acquire();
+        Thread.sleep(1000);
         while (i < 60) {
-            boolean b = limiter.tryAcquire();
-            if (!b) {
-                Thread.sleep(1000L);
-            } else {
-                System.out.println(i + "-" + System.currentTimeMillis());
+//            boolean b = limiter.tryAcquire();
+//            if (!b) {
+//                Thread.sleep(100L);
+//            } else {
+//                System.out.println(i + "-" + DateUtils.formatDateForMillSec(new Date()));
+//                ++i;
+//            }
+            limiter.acquire();
+            System.out.println(i + "-" + DateUtils.formatDateForMillSec(new Date()));
                 ++i;
-            }
         }
 
     }
