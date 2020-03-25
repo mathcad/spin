6a7b13e94f26610c3da0f0251bc4954f1c485d4b
mathcat <mathnet@qq.com>
2020-03-25 13:15:07 +0800

    删除SessionManager机制与SecretManager机制, 改造SessionUser，由业务自行实现

--- spin-core/src/main/java/org/spin/core/session/SessionUser.java
+++ spin-core/src/main/java/org/spin/core/session/SessionUser.java
@@ -1,33 +1,90 @@
 package org.spin.core.session;
 
+import org.spin.core.Assert;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.function.Supplier;
 
 /**
  * 在线用户接口
  * <p>
  * 用户实体应实现此接口,并且保证可以被正确地序列化<br>
- * 登录成功后，应通过
- * {@link SessionManager#setCurrentUser(SessionUser)}
- * 将用户实体存入session
+ * 用户应该将自定义的SessionUser实现的提供者注册给SessionUser类
  * </p>
  *
  * @author xuweinan
  * @version 1.0
  * @see Serializable
- * @deprecated 不建议再使用，有Session请自行选择相关框架
  */
-@Deprecated
-public interface SessionUser extends Serializable {
-    Long getId();
+public abstract class SessionUser<PK extends Serializable> implements Serializable {
+    private static Supplier<? extends SessionUser<? extends Serializable>> userSupplier = null;
 
-    String getUserName();
+    public static <PK extends Serializable> void registerSupplier(Supplier<? extends SessionUser<PK>> supplier) {
+        userSupplier = supplier;
+    }
 
-    boolean isActive();
+    /**
+     * 获取当前线程上绑定的用户
+     *
+     * @param <PK> id类型参数
+     * @param <S>  SessionUser类型参数
+     * @return 当前用户
+     */
+    @SuppressWarnings("unchecked")
+    public static <PK extends Serializable, S extends SessionUser<PK>> S getCurrent() {
+        if (null == userSupplier) {
+            return null;
+        } else {
+            return (S) userSupplier.get();
+        }
+    }
 
-    LocalDateTime getLoginTime();
+    /**
+     * 获取当前线程上绑定的用户, 如果不存在, 抛出异常
+     *
+     * @param <PK> id类型参数
+     * @param <S>  SessionUser类型参数
+     * @param msg  当前用户不存在时的出错提示,默认为"当前用户没有登录"
+     * @return 当前用户
+     */
+    public static <PK extends Serializable, S extends SessionUser<PK>> S getCurrentNonNull(String... msg) {
+        String nonMsg = msg == null || msg.length == 0 ? "当前用户没有登录" : msg[0];
+        return Assert.notNull(SessionUser.<PK, S>getCurrent(), nonMsg);
+    }
 
-    Serializable getSessionId();
+    /**
+     * 获取当前用户ID
+     *
+     * @return 用户ID
+     */
+    public abstract PK getId();
 
-    void setSessionId(Serializable sessionId);
+    /**
+     * 获取当前用户名称
+     *
+     * @return 用户名称
+     */
+    public abstract String getName();
+
+    /**
+     * 获取当前用户登录时间
+     *
+     * @return 登录时间
+     */
+    public abstract LocalDateTime getLoginTime();
+
+    /**
+     * 获取当前的Session ID
+     *
+     * @return Session ID
+     */
+    public abstract String getSessionId();
+
+    /**
+     * 获取当前用户登录IP
+     *
+     * @return 登录IP
+     */
+    public abstract String getLoginIp();
 }
